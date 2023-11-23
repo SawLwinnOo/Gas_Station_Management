a447b3d18f140b5197b975268d396377025d8a4e
SawLwinnOo <sawlwinnoo01@gmail.com>
2023-11-23 16:54:24 +0630

    docs

---
+++ docs/index.md
@@ -0,0 +1,42 @@
+# Station Management
+
+The Gas Station Management System is developed for a smooth operation of a gas station and 
+ensuring efficient customer service.
+
+### **Key Highlights**
+
+- Booking
+- Station Management
+- Fleet Management
+- Booking List
+
+### **Booking**
+Our booking model for station management is  designed for various station services.
+<br><br>
+<img src="img/booking_tree.png" alt="Booking Tree View" style="width: auto;"/>
+<img src="img/booking_form.png" alt="Booking Form View" style="width: auto;"/>
+
+### **Station Management**
+
+Our station management model offers a centralized solution for overseeing all station functions, 
+including employee station assignments and lane management.<br>Go to **Confiugration>>Station** menu<br><br>
+
+<img src="img/station_tree.png" alt="Station Tree View" style="width: auto;"/>
+<br><br>
+<img src="img/station_form.png" alt="Station Form View" style="width: auto;"/>
+<br> In our station lane model, we have incorporated features to efficiently manage station operations, 
+including both the fueling lane and gas pump facilities
+<img src="img/lane_tree.png" alt="Booking Lane Tree View" style="width: auto;"/>
+<img src="img/lane_form.png" alt="Booking Lane Form View" style="width: auto;"/>
+
+### **Fleet Management**
+
+In the Fleet model, you can initiate all vehicle processes, select drivers, and 
+provide comprehensive details about the engine.
+<br><br>For more details **Configuration >> Car** menu, <br><br>
+<img src="img/kanban_car.png" alt="Car Kanban View" style="width: auto;"/>
+<img src="img/car_form.png" alt="Car Form View" style="width: auto;"/>
+
+### **Booking List**
+This menu is designed for regular search, booking, and deletion of bookings using the Odoo client action implemented in Owl JS.<br><br>
+<img src="img/booking_list.png" alt="Booking Lane Form View" style="width: auto;"/>
--- station_management/static/src/station_management/booking_list.js
+++ station_management/static/src/station_management/booking_list.js
@@ -7,15 +7,12 @@ import { Component, onWillStart, useState, useRef} from "@odoo/owl";
 class BookingList extends Component {
 
     setup() {
-
-        this.booing= ['name',"customer",'car_id','station_id','booking_date','state']
+        this.model = "station.booking"
+        this.booing= ['name',"customer",'car_id','station_id','booking_date','state'];
         this.orm = this.env.services.orm;
         this.searchInput = useRef('search-input')
         this.state = useState({
-            BookingList: [
-                {'id':1, 'name': "a", 'customer': "Name"},
-                {'id':2, 'name': "b", 'customer': "Get"},
-            ]
+            BookingList: []
         })
 
         onWillStart(async () =>{
@@ -25,17 +22,33 @@ class BookingList extends Component {
 
     async GetAllBooking(){
 
-        this.state.BookingList = await this.orm.searchRead("station.booking",[], this.booing);
-        console.log(this.state.BookingList)
+        this.state.BookingList = await this.orm.searchRead(this.model,[], this.booing);
+        // console.log(this.state.BookingList)
 
     }
 
     async searchBooking(){
+
+        // get input text
         var text =this.searchInput.el.value
-        this.state.BookingList = await this.orm.searchRead("station.booking",[['name', 'ilike', text]], this.booing);
-        console.log(this.searchInput.el.value);
+        // search in the model
+        this.state.BookingList = await this.orm.searchRead(this.model,[['name', 'ilike', text]], this.booing);
+        console.log('text', text);
         console.log("search..");
     }
+
+    async deleteBooking(booking){
+        await this.orm.unlink(this.model,[booking.id])
+        await this.GetAllBooking()
+
+    }
+
+    async changeState(e, booking){
+        await this.orm.write(this.model, [booking.id], {"state": e.target.title})
+        // console.log('event..', e.target.title)
+        // console.log('booking..', booking)
+    }
+
 }
 
 BookingList.template = "station_management.booking_list";
